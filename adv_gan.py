e5b4aa854736b0cb2716ca8d2684348ab1c501af
Niharika Jain <niharikajain.az@gmail.com>
2019-07-06 14:10:25 -0700

    add target classifier and change generator to produce perturbation to add to original images

--- adv_gan.py
+++ adv_gan.py
@@ -2,18 +2,19 @@ from __future__ import print_function
 import keras
 from keras.datasets import mnist
 from keras.models import Sequential
+from keras.utils import to_categorical
 from keras import layers, Model
 from keras.layers import Input, Dense, Dropout, Flatten, Reshape, Activation, Lambda, LeakyReLU
 from keras.layers import Conv2D, AveragePooling2D, Conv2DTranspose, BatchNormalization
 from keras_contrib.layers.normalization.instancenormalization import InstanceNormalization
 from keras.optimizers import Adam, SGD
-from keras.preprocessing.image import ImageDataGenerator
-from keras.preprocessing.image import img_to_array, load_img
 from keras.metrics import binary_accuracy
 from keras import backend as K
 import os, cv2, re, random
 import numpy as np
 import matplotlib.pyplot as plt
+from numpy.random import seed
+from tensorflow import set_random_seed
 
 class DCGAN():
 
@@ -36,14 +37,18 @@ class DCGAN():
         self.D.compile(loss=keras.losses.binary_crossentropy, optimizer = optimizer_d, metrics=[self.custom_acc])
         self.D.summary()
 
-        self.stacked = Model(inputs=inputs, outputs=[self.G(inputs), self.D(self.G(inputs))])
-        self.stacked.compile(loss=[self.generator_loss, keras.losses.binary_crossentropy], optimizer = optimizer_g)
-        self.stacked.summary()
+        outputs = self.build_target(self.G(inputs))
+        self.target = Model(inputs, outputs)
+        self.target.compile(loss=keras.losses.categorical_crossentropy, optimizer=keras.optimizers.Adam(), metrics=['accuracy'])
+        self.target.summary()
 
+        self.stacked = Model(inputs=inputs, outputs=[self.G(inputs), self.D(self.G(inputs)), self.target(self.G(inputs))])
+        self.stacked.compile(loss=[self.generator_loss, keras.losses.binary_crossentropy, keras.losses.binary_crossentropy], optimizer = optimizer_g)
+        self.stacked.summary()
 
     def generator_loss(self, y_true, y_pred):
         return K.mean(K.maximum(K.sqrt(K.sum(K.square(y_pred - y_true), axis=-1)) - 0.3, 0), axis=-1)
-        #||G(x) - x||_2 - c, where c is user-defined. Here it is set to 10
+        #||G(x) - x||_2 - c, where c is user-defined. Here it is set to 0.3
 
     def custom_acc(self, y_true, y_pred):
         return binary_accuracy(K.round(y_true), K.round(y_pred))
@@ -107,17 +112,31 @@ class DCGAN():
         G = Conv2D(1, 3, padding='same')(G)
         G = InstanceNormalization()(G)
         G = Activation('relu')(G)
+        G = layers.add([G, inputs])
 
         return G
 
-    def get_batches(self, start, end, x_train):
+    def build_target(self, inputs):
+        f = Conv2D(64, 5, padding='same', activation='relu')(inputs)
+        f = Conv2D(64, 5, padding='same', activation='relu')(f)
+        f = Dropout(0.25)(f)
+        f = Flatten()(f)
+        f = Dense(128, activation='relu')(f)
+        f = Dropout(0.5)(f)
+        f = Dense(2, activation='softmax')(f)
+        return f
+
+
+
+    def get_batches(self, start, end, x_train, y_train):
         x_batch = x_train[start:end]
         Gx_batch = self.G.predict_on_batch(x_batch)
-        return x_batch, Gx_batch
+        y_batch = y_train[start:end]
+        return x_batch, Gx_batch, y_batch
 
 
     def train_D_on_batch(self, batches):
-        x_batch, Gx_batch = batches
+        x_batch, Gx_batch, _ = batches
 
         #for each batch:
             #predict noise on generator: G(z) = batch of fake images
@@ -133,28 +152,43 @@ class DCGAN():
         return d_loss #(loss, accuracy) tuple
 
 
-    def train_stacked_on_batch(self, x_batch):
+    def train_stacked_on_batch(self, batches):
+        x_batch, _, y_batch = batches
+        flipped_y_batch = 1.-y_batch
+
         #for each batch:
             #train fake images on discriminator: D(G(z)) = update G params per D's classification for fake images
 
         #Update only G params
         self.D.trainable = False
-        stacked_loss = self.stacked.train_on_batch(x_batch, [x_batch, np.ones((len(x_batch), 1))] )
+        self.target.trainable = False
+        stacked_loss = self.stacked.train_on_batch(x_batch, [x_batch, np.ones((len(x_batch), 1)), to_categorical(flipped_y_batch)] )
+        #stacked_loss = self.stacked.train_on_batch(x_batch, [x_batch, np.ones((len(x_batch), 1)), to_categorical(y_batch)] )
         #input to full GAN is original image
         #output 1 label for generated image is original image
         #output 2 label for discriminator classification is real/fake; G wants D to mark these as real=1
-        return stacked_loss #(total loss, hinge loss, gan loss) tuple
+        #output 3 label for target classification is 1/3; g wants to flip these so 1=1 and 3=0
+        return stacked_loss #(total loss, hinge loss, gan loss, adv loss) tuple
 
 
     def train_GAN(self):
-        (x_train, y_train), (_, _) = mnist.load_data()
+        (x_train, y_train), (x_test, y_test) = mnist.load_data()
         x_train = (x_train *2./255 - 1).reshape((len(x_train), 28, 28, 1)) #pixel values in range [-1., 1.] for D
         binary_indices = np.where(y_train == 1)
         x_ones = x_train[binary_indices][:6000]
+        y_ones = np.zeros((6000, 1))
         binary_indices = np.where(y_train == 3)
         x_threes = x_train[binary_indices][:6000]
+        y_threes = np.ones((6000, 1))
         x_train = np.concatenate((x_ones, x_threes)) #(12000, 28, 28, 1)
-        np.random.shuffle(x_train)
+        y_train = np.concatenate((y_ones, y_threes)) #1=0, 3=1
+        zipped = list(zip(x_train,y_train))
+        np.random.shuffle(zipped)
+        x_train, y_train = zip(*zipped)
+        x_train = np.array(x_train)
+        y_train = np.array(y_train)
+
+        self.target.fit(x_train, to_categorical(y_train), epochs=5) #pretrain target
 
         epochs = 50
         batch_size = 128
@@ -169,42 +203,54 @@ class DCGAN():
             for batch in range(num_batches - 1):
                 start = batch_size*batch_index
                 end = batch_size*(batch_index+1)
-                batches = self.get_batches(start, end, x_train)
+                batches = self.get_batches(start, end, x_train, y_train)
                 self.train_D_on_batch(batches)
+                self.train_stacked_on_batch(batches)
                 batch_index += 1
-                self.train_stacked_on_batch(batches[0])
+
 
             start = batch_size*batch_index
             end = len(x_train)
-            batches = self.get_batches(start, end, x_train)
+            x_batch, Gx_batch, y_batch = self.get_batches(start, end, x_train, y_train)
 
-            (d_loss, d_acc) = self.train_D_on_batch(batches)
-            (g_loss, hinge_loss, gan_loss) = self.train_stacked_on_batch(batches[0])
+            (d_loss, d_acc) = self.train_D_on_batch((x_batch, Gx_batch, y_batch))
+            (g_loss, hinge_loss, gan_loss, adv_loss) = self.train_stacked_on_batch((x_batch, Gx_batch, y_batch))
 
+            target_acc = self.target.test_on_batch(Gx_batch, to_categorical(y_batch))[1]
+            target_predictions = self.target.predict_on_batch(Gx_batch) #(96,2)
 
-            print("Discriminator -- Loss:%f\tAccuracy%.2f%%\nGenerator -- Loss:%f\nHinge Loss: %f" %(d_loss, d_acc*100., gan_loss, hinge_loss))
+            misclassified = np.where(y_batch.reshape((len(x_train) % batch_size, )) != np.argmax(target_predictions, axis=1))[0]
+            print(np.array(misclassified).shape)
+            print(misclassified)
 
+            print("Discriminator -- Loss:%f\tAccuracy:%.2f%%\nGenerator -- Loss:%f\nHinge Loss: %f\nTarget Loss: %f\tAccuracy:%.2f%%" %(d_loss, d_acc*100., gan_loss, hinge_loss, adv_loss, target_acc*100.))
+
+            if epoch == 0:
+                self.save_generated_images("orig", x_batch, 'images')
             if epoch % 5 == 0:
-                self.save_generated_images(epoch, batches[1])
+                self.save_generated_images(str(epoch), Gx_batch, 'images')
+                self.save_generated_images(str(epoch), Gx_batch[misclassified], 'misclass')
 
 
-    def save_generated_images(self, epoch, Gx_batch):
-        Gx_batch = Gx_batch.reshape(Gx_batch.shape[0], self.img_width, self.img_height)
+    def save_generated_images(self, filename, batch, dir):
+        batch = batch.reshape(batch.shape[0], self.img_width, self.img_height)
         rows, columns = 5, 5
 
         fig, axs = plt.subplots(rows, columns)
         cnt = 0
         for i in range(rows):
             for j in range(columns):
-                axs[i,j].imshow((Gx_batch[cnt] + 1)/2., interpolation='nearest', cmap='gray')
+                axs[i,j].imshow((batch[cnt] + 1)/2., interpolation='nearest', cmap='gray')
                 axs[i,j].axis('off')
                 cnt += 1
-        fig.savefig("images/%d.png" % epoch)
+        fig.savefig("%s/%s.png" % (dir, filename))
         plt.close()
 
 
 
 
 if __name__ == '__main__':
+    seed(5)
+    set_random_seed(1)
     dcgan = DCGAN()
     dcgan.train_GAN()
